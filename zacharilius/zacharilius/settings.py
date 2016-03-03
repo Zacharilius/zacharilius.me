eae5ab6575c6750fcf2ae6f96f9fe55eb818b6ab
zacharilius <zabensley@gmail.com>
2016-03-02 19:47:28 -0800

    Added maps app and projects app

--- zacharilius/zacharilius/settings.py
+++ zacharilius/zacharilius/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
+    'maps',
+    'projects',
 ]
 
 MIDDLEWARE_CLASSES = [
--- zacharilius/zacharilius/urls.py
+++ zacharilius/zacharilius/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('portfolio.urls')),
+    url(r'^$', include('portfolio.urls')),    
+    url(r'^maps/', include('maps.urls')),
+    url(r'^projects/', include('projects.urls')),
     url(r'^admin/', admin.site.urls),
 ]
