78fd8cbf30dcc64aa8d2d96809e597d937cd8d19
zacharilius <zabensley@gmail.com>
2016-03-05 08:09:49 -0800

    Added base for resume page

--- zacharilius/zacharilius/urls.py
+++ zacharilius/zacharilius/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('portfolio.urls')),
+    url(r'^', include('portfolio.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^maps/', include('maps.urls')),
     url(r'^projects/', include('projects.urls')),
