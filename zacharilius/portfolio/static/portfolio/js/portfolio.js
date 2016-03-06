470f9d6345153ed0d267b88fb0f8b92b30c694a5
zacharilius <zabensley@gmail.com>
2016-03-06 11:35:44 -0800

    Removed bootstrap. Started using mdl lite

--- zacharilius/portfolio/static/portfolio/js/portfolio.js
+++ zacharilius/portfolio/static/portfolio/js/portfolio.js
@@ -1,10 +1,14 @@
 $(function() {
-	setupClickNav();
+	setupHeight();
 })
 
-function setupClickNav() {
-	$('.navbar.icon').click(function() {
-		$('.icon').removeClass('is-active');
-		$(this).addClass('is-active')
+function setupHeight() {
+	setPortfolioContainerHeight();
+	$('portfolio-container').resize('resize', function() {
+		setPortfolioContainerHeight();
 	})
+}
+
+function setPortfolioContainerHeight() {
+	$('.portfolio-container').height($(window).height() - 64) /* 64 is the height of the nav title */
 }
