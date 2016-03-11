a131f5e68ab3c4b414595003afebbb968a7d8506
zacharilius <zabensley@gmail.com>
2016-03-10 20:31:39 -0800

    Installed Leaflet. Formatted starbucks dots

--- zacharilius/maps/views.py
+++ zacharilius/maps/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 def maps(request):
-	#return HttpResponse("Http response")
     return render(request, "maps/maps.html", {
     	'active_tab': 'maps'
     })
--- zacharilius/zacharilius/settings.py
+++ zacharilius/zacharilius/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Installed apps
+    'leaflet',
+
     # Local apps
     'maps',
     'portfolio',
