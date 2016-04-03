436dff9f4b2a7432be996fd282be3a0ec5bd3250
zacharilius <zabensley@gmail.com>
2016-04-02 21:31:54 -0700

    Maps: Added US State map

--- zacharilius/maps/static/maps/js/maps.js
+++ zacharilius/maps/static/maps/js/maps.js
@@ -1,4 +1,39 @@
 $(function() {
+    drawStatesWithLabels(); 
+});
+
+
+
+function drawStatesWithLabels() {
+    var width = $(window).width();
+    var height = $(window).height();
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+    var path = d3.geo.path()
+        .projection(projection);
+    var svg = d3.select("#map-container").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    d3.json(window.usStateMapUrl, function(error, us) {
+      if (error) throw error;
+      svg.insert("path", ".graticule")
+          .datum(topojson.feature(us, us.objects.land))
+          .attr("class", "land")
+          .attr("d", path);
+      // svg.insert("path", ".graticule")
+      //     .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
+      //     .attr("class", "county-boundary")
+      //     .attr("d", path);
+      svg.insert("path", ".graticule")
+          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+          .attr("class", "state-boundary")
+          .attr("d", path);
+    });
+    d3.select(self.frameElement).style("height", height + "px");    
+}
+
+function drawWorldStarbucksMap() {
     var width = 960;
     var height = 480;
 
@@ -12,27 +47,27 @@ $(function() {
 
     var graticule = d3.geo.graticule();
 
-    var svg = d3.select("#map-container").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    var svg = d3.select('#map-container').append('svg')
+      .attr('width', width)
+      .attr('height', height);
 
-  svg.append("path")
-      .datum(graticule)
-      .attr("class", "graticule")
-      .attr("d", path);
+    svg.append('path')
+        .datum(graticule)
+        .attr('class', 'graticule')
+        .attr('d', path);
 
     d3.json(window.worldMapUrl, function(error, world) {
         if (error) throw error;
 
-        svg.insert("path", ".graticule")
+        svg.insert('path', '.graticule')
             .datum(topojson.feature(world, world.objects.land))
-            .attr("class", "land")
-            .attr("d", path);
+            .attr('class', 'land')
+            .attr('d', path);
 
-        svg.insert("path", ".graticule")
+        svg.insert('path', '.graticule')
             .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-            .attr("class", "boundary")
-            .attr("d", path);
+            .attr('class', 'boundary')
+            .attr('d', path);
     });
 
 
@@ -41,18 +76,28 @@ $(function() {
         if (error) {
             throw error;
         }
+        var circle = svg.selectAll('circle')
+            .data(starbucks, function(d) { return d; });
+
+        circle.enter().append('circle')
+            .attr('fill', 'blue')
+            .attr('r', 1)
+            .attr('transform', function(d, i) {return 'translate(' + projection([d.longitude, d.latitude]) + ')';})
+            .attr('d', function(d, i) {return d});
+
+        circle.exit().remove();
 
         for (i in starbucks) {
             latitude = starbucks[i].latitude
             longitude = starbucks[i].longitude
             if (latitude || longitude) {
-                svg.append("circle")
-                  .attr("fill", "blue")
-                  .attr("r",1)
-                  .attr("transform", function() {return "translate(" + projection([longitude, latitude]) + ")";});
+                svg.append('circle')
+                  .attr('fill', 'blue')
+                  .attr('r',1)
+                  .attr('transform', function() {return 'translate(' + projection([longitude, latitude]) + ')';});
             }
         }
     })
 
-    d3.select(self.frameElement).style("height", height + "px");
-});
+    d3.select(self.frameElement).style('height', height + 'px');
+}
