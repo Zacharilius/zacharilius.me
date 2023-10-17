765f25ce3bddd7c6e904fede600a6dc06460393d
Zachary Bensley <zabensley@Zacharys-MacBook-Pro.local>
2023-10-16 21:02:08 -0400

    Add projects.

---
+++ src/Projects/ProjectIcon/Twitter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+	Twitter
+} from 'react-bootstrap-icons';
+
+function GitHubIcon() {
+    return (
+        <Twitter
+            color='rgb(0, 132, 180)'
+        />
+    );
+}
+
+export default GitHubIcon;
