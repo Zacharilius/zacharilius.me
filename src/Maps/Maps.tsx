9697c0ff2692964277f6e7ce790be8543bb5a21b
Zachary Bensley <zabensley@gmail.com>
2023-10-23 20:42:36 -0400

    Add Seattle park/neighborhood map.

--- src/Maps/Maps.tsx
+++ src/Maps/Maps.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import SeattleParks from './SeattleParks/SeattleParks';
 
 function Maps() {
 	return (
 		<>
-            Maps
+			<h3>Seattle Park Benches</h3>
+			<p>
+				Displays GeoJson of all recorded benches in Seattle. Customizes
+				each bench style by override the default Marker to instead use a
+				circle.
+			</p>
+			<SeattleParks />
 		</>
 	);
 }
